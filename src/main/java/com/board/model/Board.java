8eb1bb59aca7bdef713dbcd31aaa6209e1be83df
debbykim <rlaekqls5408@gmail.com>
2017-01-03 20:27:41 +0900

    각 설정 주석, model 수정

--- src/main/java/com/board/model/Board.java
+++ src/main/java/com/board/model/Board.java
@@ -1,7 +1,16 @@
 package com.board.model;
 
+import java.util.Date;
+
 /**
  * Created by user on 2017-01-03.
  */
 public class Board {
+
+    private int num;
+    private String name;
+    private String title;
+    private Date date;
+    private String text;
+
 }
